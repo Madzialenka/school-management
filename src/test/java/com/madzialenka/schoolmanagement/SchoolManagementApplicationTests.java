526bf9098cc34e175af2fdcfaf9b02f41d743a28
Magdalena Ceglińska <m.ceglinska1999@gmail.com>
2020-12-14 17:02:00 +0100

    basic structure of application

---
+++ src/test/java/com/madzialenka/schoolmanagement/SchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madzialenka.schoolmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6cd1b2f476d2fc7e251153392f56ca7b92ee902f
Magdalena Ceglińska <m.ceglinska1999@gmail.com>
2021-03-08 12:50:24 +0100

    exportSchoolSubjectsToExcel endpoint added

--- src/main/java/com/madzialenka/schoolmanagement/service/SchoolSubjectService.java
+++ src/main/java/com/madzialenka/schoolmanagement/service/SchoolSubjectService.java
@@ -3,6 +3,7 @@ package com.madzialenka.schoolmanagement.service;
 import com.madzialenka.schoolmanagement.api.dto.GradesMeanResponseDTO;
 import com.madzialenka.schoolmanagement.api.dto.SchoolSubjectDataRequestDTO;
 import com.madzialenka.schoolmanagement.api.dto.SchoolSubjectResponseDTO;
+import org.apache.poi.ss.usermodel.Workbook;
 
 import java.util.List;
 
@@ -17,4 +18,6 @@ public interface SchoolSubjectService {
     List<SchoolSubjectResponseDTO> getSchoolSubjects(Long schoolId);
 
     GradesMeanResponseDTO getSchoolSubjectGradesMean(Long schoolId, Long schoolSubjectId);
+
+    Workbook exportSchoolSubjectsToExcel(Long schoolId);
 }
